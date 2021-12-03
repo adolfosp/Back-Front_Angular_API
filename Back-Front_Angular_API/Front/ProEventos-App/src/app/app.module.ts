c9987df5dc48ca507aa58ad4e6a3c0feffda6ceb
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2021-12-02 21:09:01 -0300

    adiciona drop down e tooltip na tela de eventos

--- Back-Front_Angular_API/Front/ProEventos-App/src/app/app.module.ts
+++ Back-Front_Angular_API/Front/ProEventos-App/src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule  } from 'ngx-bootstrap/dropdown';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
 
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { EventoService } from './services/Evento.service';
 
 @NgModule({
@@ -24,13 +29,13 @@ import { EventoService } from './services/Evento.service';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
     FormsModule
-    
-
   ],
   providers: [EventoService],
   bootstrap: [AppComponent]
