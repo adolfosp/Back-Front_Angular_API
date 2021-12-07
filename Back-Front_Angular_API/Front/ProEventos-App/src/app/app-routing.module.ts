fed2e02c9faa34a2b0dfcb47086092d4b58c1e1a
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2021-12-06 21:53:41 -0300

    adiciona rotas

--- Back-Front_Angular_API/Front/ProEventos-App/src/app/app-routing.module.ts
+++ Back-Front_Angular_API/Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { PalestrantesComponent } from './componentes/palestrantes/palestrantes.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { ContatosComponent } from './componentes/contatos/contatos.component';
+import { EventosComponent } from './componentes/eventos/eventos.component';
+
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'eventos', component: EventosComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'contato', component: ContatosComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'palestrantes', component: PalestrantesComponent},
+  {path: '**', redirectTo: 'dashboard', pathMatch :'full'},
+  {path: '', redirectTo:'dashboard', pathMatch :'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
