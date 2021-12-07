843fbf71ee462f807d8e69c00d8c6cbbec8d31d6
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2021-12-07 20:28:10 -0300

    personaliza componente titulo

--- Back-Front_Angular_API/Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Back-Front_Angular_API/Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -8,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TituloComponent implements OnInit {
 
   @Input() titulo : string = "";
+  @Input() subtitulo : string = "Desde 2021";
+  @Input() iconClass : string = "fa fa-user";
+  @Input() botaoListar : boolean = false;
 
   constructor() { }
 
