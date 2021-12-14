b70be735d1e9d5a5e7c3581da9fcc31c175bf14a
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2021-12-13 21:40:39 -0300

    form control

--- Back-Front_Angular_API/Front/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Back-Front_Angular_API/Front/ProEventos-App/src/app/componentes/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -13,17 +13,18 @@ export class EventoDetalheComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.Validacao();
   }
 
   public Validacao() : void{
     this.form = new FormGroup({
-      local: new FormControl(),
-      dataEvento: new FormControl(),
-      tema: new FormControl(),
-      qunatidadePessoas: new FormControl(),
-      imagemURL: new FormControl(),
+      local: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      dataEvento: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      tema: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      quantidadePessoas: new FormControl('', Validators.max(120.000)),
+      imagemURL: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
       telefone: new FormControl(),
-      email: new FormControl()
+      email: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)])
 
     });
   }
